42e9ba13f9bb2229a3959c008e3809a0277751e5
andritox55 <andresww.55@hotmail.com>
2023-10-17 19:27:20 +0200

    Add project files.

---
+++ OOP/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Circle
+    {
+
+        // variabeln som innehåller värdet för den valda radius
+        int _radius;
+
+        //konstruktor av klassen
+        public Circle(int radius)
+        {
+            _radius = radius;
+        }
+
+        // metoden för att räkna ur arean
+        public int GetArea()
+        {
+            int area = (int)(_radius * _radius * Math.PI);
+            return area;
+        }
+        
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // skapa instansen med värdet
+            Circle circle = new Circle(5);
+
+            // beräkna arean med att anropa metoden
+            int area = circle.GetArea();
+
+            Console.WriteLine(area);
+        }
+    }
+}
